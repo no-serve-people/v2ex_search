c4555ce7e6b17a8d6dd289a4e397c01808408035
ixingjue <826739558@qq.com>
2018-05-06 12:18:50 +0800

    提交

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -10,7 +10,7 @@
 
     <title>氢搜索后台- Login</title>
 
-    <link rel="stylesheet" href="http://fonts.lug.ustc.edu.cn/css?family=Arimo:400,700,400italic">
+    <link rel="stylesheet" href="https://fonts.lug.ustc.edu.cn/css?family=Arimo:400,700,400italic">
     <link rel="stylesheet" href="{{ asset('assets/css/fonts/linecons/css/linecons.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/fonts/fontawesome/css/font-awesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
